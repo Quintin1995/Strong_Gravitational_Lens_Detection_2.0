177452b91a4da25dcc7c3e564a47c71c4355ad31
quintin <qvlohuizen@live.nl>
2020-07-31 11:50:20 +0200

    ready for 24000 chunks experiment

--- compare_results.py
+++ compare_results.py
@@ -320,9 +320,9 @@ root_models = "models"                              # Where the experiment folde
 ## Set experiment folder
 # experiment_folder = "experiment1_normalization"
 # experiment_folder = "experiment2_avg_pooling"
-# experiment_folder = "experiment3_chunk_amount"
+experiment_folder = "experiment3_chunk_amount"
 # experiment_folder = "experiment4_learning_rate"
-experiment_folder = "experiment5_resnetX"
+# experiment_folder = "experiment5_resnetX"
 
 root_models = os.path.join(root_models, experiment_folder)
 
@@ -334,23 +334,23 @@ models = [
     # "07_19_2020_13h_53m_16s_norm_adaptHistEq"           #normalization
     # "07_18_2020_15h_36m_29s_no_global_avg_pooling",       #global avg pooling
     # "07_18_2020_15h_36m_29s_yes_global_avg_pooling-Base", #global avg pooling
-    # "07_17_2020_13h_47m_10s_100PercChunks",             #chunk amount
-    # "07_19_2020_13h_53m_35s_50Percent_chunks",          #chunk amount
-    # "07_19_2020_13h_53m_35s_75Percent_chunks",          #chunk amount
-    # "07_19_2020_13h_53m_36s_25Percent_chunks",          #chunk amount
-    # "07_27_2020_14h_11m_11s_chunkAmount12000"           #chunk amount
+    "07_17_2020_13h_47m_10s_100PercChunks",             #chunk amount
+    "07_19_2020_13h_53m_35s_50Percent_chunks",          #chunk amount
+    "07_19_2020_13h_53m_35s_75Percent_chunks",          #chunk amount
+    "07_19_2020_13h_53m_36s_25Percent_chunks",          #chunk amount
+    "07_27_2020_14h_11m_11s_chunkAmount12000"           #chunk amount
     # "07_17_2020_14h_13m_09s_learning_rate_001",         #learning rate
     # "07_17_2020_13h_47m_10s_learning_rate_0001",        #learning rate
     # "07_19_2020_13h_54m_04s_learning_rate_00001",       #learning rate
-    "07_17_2020_13h_47m_10s_resnet18",                  #resnetX
-    "07_27_2020_13h_55m_24s_resnet50",                   #resnetx
-    "resnet_single_newtr_last_last_weights_only"          #enrico baseline
+    # "07_17_2020_13h_47m_10s_resnet18",                  #resnetX
+    # "07_27_2020_13h_55m_24s_resnet50",                   #resnetx
+    # "resnet_single_newtr_last_last_weights_only"          #enrico baseline
 ]
 json_comp_key           = "model_name"              # is the label in generated plots
 verbatim = False
 
 ### 0 - Error Plot of given Models
-do_show_error_plot      = False
+do_show_error_plot      = True
 error_window_size       = 150     # avg window size
 ytop                    = 10.0    # Error plot y upper-limit in percentage
 ybottom                 = 4.00    # Error plot y bottom-limit in percentage
@@ -358,13 +358,13 @@ ybottom                 = 4.00    # Error plot y bottom-limit in percentage
 
 ### 1a - Overfit plot ###
 # Shows a plot where the loss is average over time. (also plots the difference between training- and validation loss.)
-do_show_overfit_plot    = False
+do_show_overfit_plot    = True
 window_size             = 50      # Determines over how many datapoints the average is taken. (window size in the future. (x = avg(next 50 datapoints) if x=50))
 
 
 ### 2a - Barrage of plots ###
 # Show a barrage of plots to the user defined in plots_to_show list.
-show_all_step4_plots    = False
+show_all_step4_plots    = True
 plots_to_show = {
     # "loss",
     # "binary_accuracy",
