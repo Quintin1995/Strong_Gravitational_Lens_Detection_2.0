9feb4c96ac4fc0ee6d3d012fff25b6123af4ad2c
quintin <qvlohuizen@live.nl>
2020-09-21 10:39:11 +0200

    the soft f beta validation metric seems to start at one, this could be because i tried to train it with an uninformative learner as the blog suggests.

--- f_beta_soft_metric.py
+++ f_beta_soft_metric.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 class SoftFBeta:
 
-    
+
     def __init__(self, beta = 0.17):
         tf.compat.v1.disable_eager_execution()
         self.beta = beta
@@ -17,7 +17,7 @@ class SoftFBeta:
         return tf.reduce_sum(y_true * y_pred) / (tf.reduce_sum(y_true) + K.epsilon())
 
 
-    def f_beta_loss(self, y_true, y_pred):
+    def f_beta_soft(self, y_true, y_pred):
         y_true = tf.cast(y_true, tf.float32)
         precision   = self._precision(y_true, y_pred)
         recall      = self._recall(y_true, y_pred)
