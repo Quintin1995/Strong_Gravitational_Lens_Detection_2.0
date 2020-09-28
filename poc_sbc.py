269e6d1256dd4d77fd4849a7883c09929ab483cd
quintin <qvlohuizen@live.nl>
2020-09-28 16:36:48 +0200

    Applied floodfill, maybe its better to use a simple circular mask around the centre? based on the maximum einstein radii?

--- poc_sbc.py
+++ poc_sbc.py
@@ -9,17 +9,21 @@ import numpy as np
 from Parameters import Parameters
 from utils import load_settings_yaml, show_random_img_plt_and_stats
 import matplotlib.pyplot as plt
+from skimage.segmentation import flood, flood_fill
 
-# Proof of concept of surface brightness contouring
+
+def do_floodfill(img, tolerance=0):
+    filled_img = flood_fill(img, (20, 20), 0, tolerance=tolerance)
+    return filled_img
 
 
-def scale_img_dist_from_centre(img, scale_fac):
+def scale_img_dist_from_centre(img, x_scale):
     nx, ny = img.shape
     x = np.arange(nx) - (nx-1)/2.  # x an y so they are distance from center, assuming array is "nx" long (as opposed to 1. which is the other common choice)
     y = np.arange(ny) - (ny-1)/2.
     X, Y = np.meshgrid(x, y)
     d = np.sqrt(X**2 + Y**2)
-    return img * np.exp(-1*d/25)
+    return img * np.exp(-1*d/x_scale)
 
 
 def apply_gaussian_kernel(numpy_array, kernel = None):
@@ -27,11 +31,16 @@ def apply_gaussian_kernel(numpy_array, kernel = None):
         kernel = np.array([[1,2,1],
                             [2,4,2],
                             [1,2,1]]) * (1/16)
-    for i in range(numpy_array.shape[0]):
-        img = np.squeeze(numpy_array[i])
-        conv_img = ndimage.convolve(img, kernel, mode='constant', cval=0.0) 
-        numpy_array[i] = np.expand_dims(conv_img, axis = 2)
-    return x
+    if numpy_array.ndim != 2:
+        for i in range(numpy_array.shape[0]):
+            img = np.squeeze(numpy_array[i])
+            conv_img = ndimage.convolve(img, kernel, mode='constant', cval=0.0) 
+            numpy_array[i] = np.expand_dims(conv_img, axis = 2)
+        return numpy_array
+    if numpy_array.ndim == 2:
+        return ndimage.convolve(numpy_array, kernel, mode='constant', cval=0.0)
+
+
 
 ################################# script #################################
 
@@ -91,37 +100,37 @@ for i in range(x.shape[0]):
     Hmin,Hmax = 3,60
     rr = 0.45
 
-    #area filter
+    # Filter 1
+    # Area filter
     area = 45
     mxt.areaOpen(area)
 
     # Computing bouding-box lengths from the
-    # attributes stored in NA
     dx = mxt.node_array[7,:] - mxt.node_array[6,:]
     dy = mxt.node_array[10,:] - mxt.node_array[9,:]
     RR = 1.0 * area / (dx*dy)
-    # cx = mxt.node_array[6,:] + dx/2
-    # cy = mxt.node_array[9,:] + dy/2
 
-    # extinction filter
-    # Computes the area extinction values
-    # n = 1
-    # area = mxt.node_array[3,:]
-    # area_ext = mxt.computeExtinctionValues(area, "area")
-    # mxt.extinctionFilter(area_ext,n)
-
+    # Filter 2
     # Selecting nodes that fit the criteria
     nodes = (dx > Hmin) & (dx<Hmax) & (dy>Wmin) & (dy<Wmax) & ((RR>1.1) | (RR<0.9))
 
-    # # Filtering
+    # Filtering
     mxt.contractDR(nodes)
     print("Max-tree build time: %fs" %t)
     print("Number of max-tree nodes: %d" %mxt.node_array.shape[1])
     print("Number of max-tree leaves: %d" %(mxt.node_array[1,:] == 0).sum())
     img_filtered = mxt.getImage()
 
-    img_filtered = scale_img_dist_from_centre(img_filtered, scale_fac=20)
+    # Filter 3
+    img_filtered = scale_img_dist_from_centre(img_filtered, x_scale=30)
 
+    # Filter 4
+    img_filtered = apply_gaussian_kernel(img_filtered)
+
+    # Filter 5
+    img_filtered = do_floodfill(img_filtered, tolerance=20)
+
+    # Add imgs to list for plotting
     imgs = [img, img_filtered]
 
     fig=plt.figure(figsize=(8,8))
