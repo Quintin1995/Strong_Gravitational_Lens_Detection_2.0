eb17881cfc1641e184423c7d7f7d7c6f6c006cf9
quintin <qvlohuizen@live.nl>
2020-09-29 17:40:15 +0200

    square centered crop can be taken.

--- poc_sbc.py
+++ poc_sbc.py
@@ -11,6 +11,7 @@ from utils import load_settings_yaml, show_random_img_plt_and_stats
 import matplotlib.pyplot as plt
 from skimage.segmentation import flood, flood_fill
 
+
 # Perform the floodfill operation on pixel (20,20)
 def do_floodfill(img, tolerance=0):
     filled_img = flood_fill(img, (20, 20), 0, tolerance=tolerance)
@@ -53,10 +54,10 @@ def apply_gaussian_kernel(numpy_array, kernel = None):
 # do_sgf:           Scale-on-distance with Gaussian kernel convolution and Floodfill
 #   This basically removes a whole bunch of noise objects in the image.
 # do_square_crop:   take centering square out of the image, where the centering
-#   square has emperically been determined to be: (73, 73) pixels. All lensing 
+#   square has emperically been determined to be: (74, 74) pixels. All lensing 
 #   features should fall within this range.
 # do_circular_crop: Basically does the same thing as do_square_crop. However,
-#   all pixels outside the centered circle with radius 73 will be set to a zero value.
+#   all pixels outside the centered circle with radius 74 will be set to a zero value.
 # x_scale:          Parameter of brighness scaling based on distance. The higher the
 #   more distant objects (from centre) their brighness is retained. The lower, the
 #   more distant objects (from centre) their brightness is reduced.
@@ -66,27 +67,32 @@ def apply_gaussian_kernel(numpy_array, kernel = None):
 # the original image.
 def max_tree_segmenter(numpy_array, do_square_crop=False, do_circular_crop=False, do_sgf=False, x_scale=30, tolerance=20, area_th=45):
 
+    print("\nmax tree segmenter, incoming shape={}".format(numpy_array.shape))
+
     #structuring element, connectivity 8, for each pixel all 8 surrounding pixels are considered.
     connectivity_kernel = np.ones(shape=(3,3), dtype=bool)
 
     # Bounding-box criteria, Area size threshold, Squeare crop size
-    Wmin,Wmax           = 3,60                  # Emperically determined (ED)
-    Hmin,Hmax           = 3,60                  # ED
+    Wmin,Wmax           = 3, 60                 # Emperically determined (ED)
+    Hmin,Hmax           = 3, 60                 # ED
     area_threshold      = area_th               # ED: size of a component
-    square_crop_size    = 73
-    t                   = time.time()           # record time
+    square_crop_size    = 74                    # Size of square crop. shape=(74, 74)
+    r                   = square_crop_size//2   # Radius
+    t                   = time.time()           # Record time
 
     # create array where filtered data will be stored
     if do_square_crop:
-        f_dat = np.zeros((numpy_array.shape[0], square_crop_size, square_crop_size, 1), dtype='uint16')
+        cx = numpy_array.shape[1]//2
+        cy = numpy_array.shape[2]//2
+        f_dat = numpy_array[:, cx-r:cx+r, cy-r:cy+r, :]
     else:
-        f_dat = np.zeros((numpy_array.shape[0], numpy_array.shape[1], numpy_array.shape[2], 1), dtype='uint16')
+        f_dat = np.copy(numpy_array)        # work with a copy for now
 
     # loop over all images
-    for i in range(numpy_array.shape[0]):
+    for i in range(f_dat.shape[0]):
 
         # Convert image format for Max-Tree
-        img = np.clip(np.squeeze(x[i,:]) * 255, 0, 255).astype('uint16')
+        img = np.clip(np.squeeze(f_dat[i,:]) * 255, 0, 255).astype('uint16')
 
         # Construct a Max-Tree
         mxt = siamxt.MaxTreeAlpha(img, connectivity_kernel)
@@ -118,42 +124,37 @@ def max_tree_segmenter(numpy_array, do_square_crop=False, do_circular_crop=False
     return f_dat
 
 
-
 ################################# script #################################
-
-
 # 1.0 - Define ArgumentParser
 parser = argparse.ArgumentParser()
 parser.add_argument("--run", help="Location/path of the run.yaml file. This is usually structured as a path.", default="runs/run_poc_sbc.yaml", required=False)
 args = parser.parse_args()
 
-
 # 2.0 - Unpack args
 yaml_path = args.run
-
 
 # 3.0 - Load all settings from .yaml file
 settings_yaml = load_settings_yaml(yaml_path)                                           # Returns a dictionary object.
 params = Parameters(settings_yaml, yaml_path)
 params.data_type = np.float32 if params.data_type == "np.float32" else np.float32       # This must be done here, due to the json, not accepting this kind of if statement in the parameter class.
 
-
 # 4.0 - Create Custom Data Generator
 if False:
-    mul = 75
+    mul = 7
     random.seed(325*mul)
     np.random.seed(789*mul)
 dg = DataGenerator(params)
 
 
 # 5.0 - Create Data
-x, y = dg.load_chunk(params.chunksize, dg.Xlenses_train, dg.Xnegatives_train, dg.Xsources_train, params.data_type, params.mock_lens_alpha_scaling)
+x_dat, y = dg.load_chunk(params.chunksize, dg.Xlenses_train, dg.Xnegatives_train, dg.Xsources_train, params.data_type, params.mock_lens_alpha_scaling)
 print(y)
-print(y.shape)
+copy_x_dat = np.copy(x_dat)
+seg_dat = max_tree_segmenter(x_dat, do_square_crop=True, do_circular_crop=False, do_sgf=True, x_scale=30, tolerance=20, area_th=45)
 
-seg_dat = max_tree_segmenter(x, do_square_crop=False, do_circular_crop=False, do_sgf=True, x_scale=30, tolerance=20, area_th=45)
-show_random_img_plt_and_stats(x, num_imgs=1, title="dat", do_plot=False, do_seed=True, seed=7846)
-show_random_img_plt_and_stats(seg_dat, num_imgs=1, title="seg dat", do_plot=False, do_seed=True, seed=7846)
-plt.show()
+for i in range(5):
+    show_random_img_plt_and_stats(copy_x_dat, num_imgs=1, title="dat", do_plot=False, do_seed=True, seed=87*i)
+    show_random_img_plt_and_stats(seg_dat, num_imgs=1, title="seg dat", do_plot=False, do_seed=True, seed=87*i)
+    plt.show()
 
 
